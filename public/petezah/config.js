0c5a30efbac2d9e2ad708285d769e81b5ca646ea
PeteZah <petezahiscool@gmail.com>
2025-05-17 13:55:52 -0400

    hopefully a youtube fix

--- public/petezah/config.js
+++ public/petezah/config.js
@@ -10,4 +10,24 @@ self.__uv$config = {
   bundle: "/petezah/bundle.js",
   config: "/petezah/config.js",
   sw: "/petezah/rizz.sw.js",
-};
+
+    /**
+     * Function to inject scripts into the doc Head
+     * @type {function}
+     * @param {URL} url - The URL for the rewrite function.
+     * @returns {string} - The script to inject.
+     */
+    inject: async (url) => {
+      if (url.host === '') {
+          return `
+                `
+      }if (url.host === "www.youtube.com") {
+          //meow
+          return `<script src="/storage/js/youtube.js"></script>`
+      }
+
+      return `
+      <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
+      `
+  },
+}
